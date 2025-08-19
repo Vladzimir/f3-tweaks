31b3d9c709f49b62db801e613fc3361c3a1a0a7d
Vladzimir <5346225+Vladzimir@users.noreply.github.com>
2025-08-19 17:55:24 +0300

    Add fastHash in Crypto

--- lib/Tweaks/Helpers/HelperCrypto.php
+++ lib/Tweaks/Helpers/HelperCrypto.php
@@ -10,6 +10,8 @@ class HelperCrypto extends Prefab
 {
     private const string HKDF_ALGO = 'sha3-512';
     private const string HMAC_ALGO = 'sha3-512';
+    private const string HASH_ALGO = 'xxh128';
+    private const int HASH_BLOCK = 16;
     private const int HKDF_HASH_LEN = 64; // sha3-512 -> 64 bytes
     public const int MAX_LENGTH = self::HKDF_HASH_LEN * 255; // 64 * 255 = 16320
 
@@ -23,13 +25,14 @@ class HelperCrypto extends Prefab
         $max = self::MAX_LENGTH;
         $chunks = (int)ceil($length / $max);
         $output = '';
+        $seed = System::SEED->get();
 
         for ($i = 0; $i < $chunks; $i++) {
             // length for this chunk
             $chunkLen = ($i === $chunks - 1) ? ($length - strlen($output)) : $max;
 
             // call hash_hkdf for this chunk
-            $chunk = hash_hkdf(self::HKDF_ALGO, $key, $chunkLen, $info . $i, System::SEED->get());
+            $chunk = hash_hkdf(self::HKDF_ALGO, $key, $chunkLen, $info . $i, $seed);
 
             $output .= $chunk;
         }
@@ -45,5 +48,22 @@ class HelperCrypto extends Prefab
         }
 
         return substr($hash, 0, $maxLength);
+    }
+
+    public function fastHash(string $data, int $length = 64, $binary = false): string
+    {
+        $num = (int)ceil($length / self::HASH_BLOCK);
+        $seed = System::SEED->get();
+        $hash = '';
+
+        for ($i = 0; $i < $num; $i++) {
+            $hash .= hash(self::HASH_ALGO, $i . $data, true, ["seed" => $seed]);
+        }
+
+        if (!$binary) {
+            $hash = Tweaks::base64()->urlSafeOriginalBase64Encode($hash);
+        }
+
+        return substr($hash, 0, $length);
     }
 }
